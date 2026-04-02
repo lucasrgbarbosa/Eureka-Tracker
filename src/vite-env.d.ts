98dafa2e486b1b4283246ac86fbe75d4313c803a
lucasrgbarbosa <lucasrgb03@gmail.com>
2026-04-02 00:00:16 -0300

    fix: Corrigindo Bugs

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
+declare module '*.module.css' {
+  const classes: { [key: string]: string };
+  export default classes;
+}
